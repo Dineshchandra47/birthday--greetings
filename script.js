c65c86b941875fad9e49d3886a91c2877d30ae66
Dineshchandra47 <119965753+Dineshchandra47@users.noreply.github.com>
2023-02-07 16:11:16 +0530

    Added script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        // emojis: ['🍫','🍫', '⚡️', '💥', '✨', '💫'],
+    }).then(() => jsConfetti.addConfetti())
+    
+})
